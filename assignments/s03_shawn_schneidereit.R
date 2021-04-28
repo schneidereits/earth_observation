622b2c79b70887c19a23bc6a0f9cb81f680d1488
schneidereits <s1637673@ed.ac.uk>
2021-04-28 12:47:44 +0200

    added ass 3

---
+++ assignments/s03_shawn_schneidereit.R
@@ -0,0 +1,130 @@
+##############################################################################
+# MSc Earth Observation Assignment [Session 2]
+# [Shawn Schneidereit]
+#############################################################################
+
+# Load the package ----
+library(raster)
+library(rgdal)
+library(tidyverse)
+
+
+
+##############################################################################
+
+# Vegetation indices and data transformations
+
+
+#############################################################################
+
+
+
+##############################################################################
+# 1) Compute vegetation indices
+#############################################################################
+
+stack_march <- stack("data/gcg_eo_s03/sr_data/LC081890252014031001T1-SC20170927101754/LC081890252014031001T1-SC20170927101754_sr_masked_crop.tif")
+stack_march[stack_march>10000] = NA
+stack_march[stack_march<0] = NA
+
+plot(stack_march)
+
+blue <- stack_march[[1]]
+green <- stack_march[[2]]
+red <- stack_march[[3]]
+nIR <- stack_march[[4]]
+SWIR <- stack_march[[5]]
+
+# NDVI
+
+ndvi <- ((nIR - red) / (nIR + red)) 
+
+print(ndvi)
+plot(ndvi)
+
+# EVI 
+
+# The correction factors for calculating EVI from Landsat data
+G=2.5
+C1=6
+C2=7.5
+L=1
+
+evi <- G * (nIR - red) / (nIR + C1 * red - C2 * blue + (L*10000))
+evi[evi<0] = 0
+
+plot(evi)
+
+stack_ndvi <-  writeRaster(tcc_stack, "~/Documents/humbolt/semester_02/earth_observation/data/gcg_eo_s03/sr_data/LC081890252014031001T1-SC20170927101754/LC081890252014031001T1-SC20170927101754_ndvi",
+                          format = "GTiff", 
+                          datatype = "INT2S",
+                          overwrite = T)
+
+stack_evi <-  writeRaster(tcc_stack, "~/Documents/humbolt/semester_02/earth_observation/data/gcg_eo_s03/sr_data/LC081890252014031001T1-SC20170927101754/LC081890252014031001T1-SC20170927101754_evi",
+                          format = "GTiff", 
+                          datatype = "INT2S",
+                          overwrite = T)
+
+##############################################################################
+# 2) Perform a Tasseled Cap transformation
+#############################################################################
+
+tcc <- matrix(c( 0.2043,  0.4158,  0.5524, 0.5741,  0.3124,  0.2303, 
+                 -0.1603, -0.2819, -0.4934, 0.7940, -0.0002, -0.1446,
+                 0.0315,  0.2021,  0.3102, 0.1594, -0.6806, -0.6109), 
+              dimnames = list(c('blue', 'green', 'red', 'nIR', 'swIR1', 'swIR2'), c('brightness', 'greenness', 'wetness')),
+              ncol = 3)
+
+print(tcc)
+
+brightness <- stack_march[[1]] * tcc[1,1] + 
+              stack_march[[2]] * tcc[2,1] + 
+              stack_march[[3]] * tcc[3,1] + 
+              stack_march[[4]] * tcc[4,1] + 
+              stack_march[[5]] * tcc[5,1] + 
+              stack_march[[6]] * tcc[6,1]
+
+
+greenness <- stack_march[[1]] * tcc[1,2] + 
+             stack_march[[2]] * tcc[2,2] + 
+             stack_march[[3]] * tcc[3,2] + 
+             stack_march[[4]] * tcc[4,2] + 
+             stack_march[[5]] * tcc[5,2] + 
+             stack_march[[6]] * tcc[6,2]
+
+
+wetness <- stack_march[[1]] * tcc[1,3] + 
+           stack_march[[2]] * tcc[2,3] + 
+           stack_march[[3]] * tcc[3,3] + 
+           stack_march[[4]] * tcc[4,3] + 
+           stack_march[[5]] * tcc[5,3] + 
+           stack_march[[6]] * tcc[6,3]
+
+tcc_stack <- stack(brightness, greenness, wetness)
+
+stack_tcc <-  writeRaster(tcc_stack, "~/Documents/humbolt/semester_02/earth_observation/data/gcg_eo_s03/sr_data/LC081890252014031001T1-SC20170927101754/LC081890252014031001T1-SC20170927101754_tcc",
+                          format = "GTiff", 
+                          datatype = "INT2S",
+                          overwrite = T)
+
+##############################################################################
+
+#Training data
+
+#############################################################################
+
+
+
+##############################################################################
+# 1) Prepare the training data collection
+#############################################################################
+
+
+##############################################################################
+# 2) Collect training data
+#############################################################################
+
+
+##############################################################################
+# 3) Explore your training data
+#############################################################################
