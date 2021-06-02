d07459a5abde4063ca91bd48f2b6909203663cd1
schneidereits <s1637673@ed.ac.uk>
2021-06-02 11:43:31 +0200

    safty commit

--- assignments/s08_shawn_schneidereit.R
+++ assignments/s08_shawn_schneidereit.R
@@ -12,7 +12,17 @@ library(rgdal)
 library(raster)
 library(randomForest)
 
+stack <- stack(paste0("data/gcg_eo_s08/spectemp/TCB/TCB_",c("iqr",
+                                                            "max",
+                                                            "mean",
+                                                            "min",
+                                                            "p25",
+                                                            "p50",
+                                                            "p75",
+                                                            "std"),".tif"))
+                                                     
 
+plot(stack)
 
 #############################################################################
 # 1) Inspect the input data
@@ -21,6 +31,25 @@ library(randomForest)
 #############################################################################
 # 2) Assess suitability of spectral-temporal metrics
 #############################################################################
+
+# Compare the temporal behavior of the two classes, identify the main
+# characteristics and differences. Please record your insights as 
+# comments in your script. Select spectral-temporal metrics that you 
+# believe capture the differences between croplands and pastures well.
+# Select at most six different metrics from the ones provided.
+
+training_data <- readOGR(dsn='data/gcg_eo_s08/training')
+
+/Users/shawn/Documents/humbolt/semester_02/earth_observation/data/gcg_eo_s08/training
+
+df <- as.data.frame(stack) 
+ 
+df_long <- df %>% 
+  pivot_longer(names_to = "variable", values_to = "value", cols = c(1:8))
+
+ggplot(df_long, aes(x=variable, y=value, color=variable)) +
+  geom_boxplot()
+
 
 #############################################################################
 # 4) Classify post-deforestation land use
